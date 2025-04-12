d62becf071d85cf963f6e64a136b143f73bac282
Cameron Eldridge <cameldridge@gmail.com>
2025-04-12 09:55:19 -0400

    fix typo

--- src/shopify/env.js
+++ src/shopify/env.js
@@ -1,6 +1,6 @@
 const { join } = require("path");
 
-const SHOPIFY_CONFIG_DIR = process.env.CONFIG_DIR ? join(process.env.CONFIG_DIR, 'shopfiy') : __dirname;
+const SHOPIFY_CONFIG_DIR = process.env.CONFIG_DIR ? join(process.env.CONFIG_DIR, 'shopify') : __dirname;
 
 module.exports.CREDENTIALS_PATH = join(SHOPIFY_CONFIG_DIR, 'credentials.json');
 module.exports.TOKEN_PATH = join(SHOPIFY_CONFIG_DIR, 'token.json');
